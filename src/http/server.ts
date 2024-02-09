59e8c8b4e2fd3ab7529559cea4b67aeff21ba211
unknown <mateus.r2000@gmail.com>
2024-02-09 19:53:02 -0300

    updates

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,25 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from 'zod';
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'Hello NLW'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+
+    return poll
 })
 
 app.listen({ port: 3333 }).then(() => {
